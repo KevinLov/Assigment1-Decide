bac3bf541a882268224c34782fa8b47d67f60750
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-22 14:35:35 +0100

    feat: CMVTest with boilerplate tests (#20)
    Participants can now insert the tests for the Lic's that they implemen.

---
+++ src/test/java/se/kth/DD2480/CMVTest.java
@@ -0,0 +1,86 @@
+package se.kth.DD2480;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CMVTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void lic0() {
+    }
+
+    @Test
+    void lic1() {
+    }
+
+    @Test
+    void lic2() {
+    }
+
+    @Test
+    void lic3() {
+    }
+
+    @Test
+    void lic4() {
+    }
+
+    @Test
+    void lic5() {
+    }
+
+    @Test
+    void lic6() {
+    }
+
+    @Test
+    void lic7() {
+    }
+
+    @Test
+    void lic8() {
+    }
+
+    @Test
+    void lic9() {
+    }
+
+    @Test
+    void lic10() {
+    }
+
+    @Test
+    void lic11() {
+    }
+
+    @Test
+    void lic12() {
+    }
+
+    @Test
+    void lic13() {
+    }
+
+    @Test
+    void lic14() {
+    }
+
+    @Test
+    void lic15() {
+    }
+
+    @Test
+    void verifyAllLics() {
+    }
+}
