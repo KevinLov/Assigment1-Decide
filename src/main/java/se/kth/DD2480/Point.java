fe6cabf20a207035317f874f3748f399e52f936c
Ahmed <ahmed.baccar@epfl.ch>
2026-01-22 16:55:12 +0100

    feat: add a method to calculate the distance between two points in the Point class.

--- src/main/java/se/kth/DD2480/Point.java
+++ src/main/java/se/kth/DD2480/Point.java
@@ -8,4 +8,8 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    double distance (Point p2){
+        return Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
+    }
 }
