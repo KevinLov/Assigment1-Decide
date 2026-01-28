ec0fc4b72da6f75d78a9a7d36c497017228d197b
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-29 00:56:52 +0100

    feat: PUV constructor and fields (#109)
    Adding constructor that takes a boolean array of size 15

--- src/main/java/se/kth/DD2480/PUV.java
+++ src/main/java/se/kth/DD2480/PUV.java
@@ -1,4 +1,14 @@
 package se.kth.DD2480;
 
 public class PUV {
+    final int SIZE = 15;
+    boolean[] arr;
+
+    public PUV(boolean[] arr) {
+        if (arr == null || arr.length != SIZE) {
+            throw new IllegalArgumentException("PUV must be atleast: " + SIZE);
+        }
+        this.arr = arr;
+    }
+
 }
