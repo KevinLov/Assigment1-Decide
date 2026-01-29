6e188a8ce0031f4ae12c26dad69009bd8c0cff3e
Teoman Köylüoglu <151879574+Laafayeette@users.noreply.github.com>
2026-01-29 03:52:51 +0100

    feat: Add Decide method (#117)
    Includes the program which is extracted into a DECIDE method.

--- src/main/java/se/kth/DD2480/Main.java
+++ src/main/java/se/kth/DD2480/Main.java
@@ -1,11 +1,44 @@
 package se.kth.DD2480;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        DECIDE();
+    }
 
+    private static void DECIDE() {
+        PUV puv = PUV.createPUV(1);
+        LCM lcm = LCM.createLCM(1);
+        Parameters params  = Parameters.createParameters(1);
+        CMV cmv = new CMV();
 
+        Point[] points0 = {
+                new Point(0, 0),   new Point(1, 0),   new Point(2, 0),  new Point(3, 0),   new Point(4, 0),   new Point(0,0),   new Point(1, 0),  new Point(0, 1),
+                new Point(10,10),  new Point(20, 10), new Point(5, 5),  new Point(-1, -1), new Point(1, 0),   new Point(5, 5),   new Point(0, 1),
+                new Point(6, 1),   new Point(7, 4),   new Point(8, 3),  new Point(9, 0),   new Point(0, 0),
+                new Point(0,0),    new Point(0,0),    new Point(2,0),   new Point(0,0),    new Point(0,2),    new Point(0, 0), new Point(2, 0), new Point(0, 1), new Point(3, 0),
+                new Point(1,0),    new Point(0, 0),   new Point(1, 0),  new Point(2, 0),   new Point(3, 0),   new Point(4, 0),
+                new Point(-1, -1), new Point(1, 0),   new Point(5, 5),  new Point(0, 0),   new Point(1, 0),   new Point(3, 0),  new Point(1, 0),
+                new Point(0, 0),   new Point(1, 0),   new Point(0.5, 0),new Point(0, 0),   new Point(-1, -1), new Point(10, 0),  new Point(0, 10),  new Point(0, 0),   new Point(3, 6),
+                new Point(0, 1),   new Point(0, 0),   new Point(1, 0),  new Point(0,0),    new Point(1,0),    new Point(10,0), new Point(0,10), new Point(0, 0), new Point(10, 0)
+        };
 
+        Point[] points1 = {
+                new Point(0,0), new Point(0,0),
+                new Point(0,0), new Point(0,0),
+                new Point(0,0), new Point(0,0)
+        };
+
+        boolean[] cmvArray = cmv.verifyAllLics(params, points1, points1.length);
+        PUM pum = new PUM();
+        boolean[][] pumArray = pum.makePUM(lcm.getArr(), cmvArray);
+        FUV fuv = new FUV(pum, puv.getArray());
+        if(fuv.launch) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
     }
 }
